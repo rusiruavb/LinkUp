a631a71eae335b9f660074e6b7f0603c223c9e33
rusiruavb <rusiruavb98@gmail.com>
2022-03-27 12:52:35 +0530

    Add profile icon to app bar

---
+++ lib/screens/applications/application_screen.dart
@@ -0,0 +1,15 @@
+import "package:flutter/material.dart";
+
+class ApplicationScreen extends StatefulWidget {
+  const ApplicationScreen({Key key}) : super(key: key);
+
+  @override
+  _ApplicationScreenState createState() => _ApplicationScreenState();
+}
+
+class _ApplicationScreenState extends State<ApplicationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
