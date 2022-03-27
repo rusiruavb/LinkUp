a6e78dcb19c28e44e8c13e8496ef5d3294bc4e9e
rusiruavb <rusiruavb98@gmail.com>
2022-03-27 14:27:08 +0530

    Implement screen to view the submitted applications

--- lib/screens/applications/application_screen.dart
+++ lib/screens/applications/application_screen.dart
@@ -1,4 +1,7 @@
-import "package:flutter/material.dart";
+import 'package:flutter/material.dart';
+import 'package:linkup/constants.dart';
+import 'package:linkup/screens/applications/application_tile.dart';
+import 'package:linkup/screens/bookmark_jobs/bookmark_job_body_card.dart';
 
 class ApplicationScreen extends StatefulWidget {
   const ApplicationScreen({Key key}) : super(key: key);
@@ -8,8 +11,44 @@ class ApplicationScreen extends StatefulWidget {
 }
 
 class _ApplicationScreenState extends State<ApplicationScreen> {
+  bool _isExpanded = false;
+  int _index = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    final orientation = MediaQuery.of(context).orientation;
+
+    return Scaffold(
+      backgroundColor: colorDarkBackground,
+      body: SingleChildScrollView(
+        child: Align(
+          alignment: Alignment.topCenter,
+          child: Container(
+            width: orientation == Orientation.landscape
+                ? size.width * 0.75
+                : size.width,
+            padding: const EdgeInsets.only(top: 5, bottom: 5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                for (var i = 0; i < 5; i++)
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: ApplicationTile(
+                      index: _index,
+                      iterateIndex: i,
+                      isExpanded: _isExpanded,
+                      title: "Job application",
+                      subTitle: "Test jon application",
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/bookmark_jobs/bookmark_jobs_screen.dart
+++ lib/screens/bookmark_jobs/bookmark_jobs_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import '../../components/side_navbar.dart';
 import '../../constants.dart';
 import 'bookmark_job_body_card.dart';
@@ -21,75 +22,76 @@ class _BookMarkedJobsScreenState extends State<BookMarkedJobsScreen> {
 
     return Scaffold(
       backgroundColor: colorDarkBackground,
-      drawer: const SideNavbar(),
       body: SingleChildScrollView(
         child: Align(
           alignment: Alignment.topCenter,
           child: Container(
-              width: orientation == Orientation.landscape
-                  ? size.width * 0.75
-                  : size.width,
-              padding: const EdgeInsets.only(top: 5, bottom: 5),
-              child: Column(
-                children: [
-                  for (var i = 0; i < 5; i++)
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 5),
-                      child: ExpansionTile(
-                        initiallyExpanded: false,
-                        collapsedBackgroundColor: colorDarkMidGround,
-                        backgroundColor: colorDarkMidGround,
-                        leading: Image.network(
-                          "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-icon-png-transparent-background-osteopathy-16.png",
-                          scale: 25,
-                        ),
-                        title: const Text(
-                          "Associate Software Engineer",
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontFamily: fontFamilySFPro,
-                            color: colorTextPrimary,
-                          ),
-                        ),
-                        subtitle: const Text(
-                          "Google In.",
-                          style: TextStyle(
-                            fontFamily: fontFamilySFPro,
-                            color: colorTextPrimary,
-                          ),
-                        ),
-                        trailing: Icon(
-                          _index == i && _isExpanded
-                              ? Icons.arrow_circle_up
-                              : Icons.arrow_circle_down,
-                          color: colorTextPrimary,
-                          size: 30,
-                        ),
-                        onExpansionChanged: (bool expanded) {
-                          setState(() {
-                            _index = i;
-                            _isExpanded = expanded;
-                          });
-                        },
-                        children: const <Widget>[
-                          BookmarkedJobBodyCard(
-                            companyLogo:
-                                "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-icon-png-transparent-background-osteopathy-16.png",
-                            companyName: "Google In.",
-                            description:
-                                "A job description not only describes the position’s responsibilities, it sets the foundation for recruiting, developing and retaining talent and also sets the stage for optimum work performance by clarifying responsibilities, expected results, and evaluation of performance. It is also an important component to maintaining an equitable compensation system and ensuring legal compliance. The document should be revisited and updated in line with the annual performance evaluation cycle.",
-                            position: "Associate Software Engineer",
-                            postImage:
-                                "https://templates.mediamodifier.com/5e9709395e1d70189ea21cd1/job-posting-linkedin-post-template.jpg",
-                            qualifications: "Test",
-                            salary: "45000",
-                            type: "Full Time",
-                          ),
-                        ],
+            width: orientation == Orientation.landscape
+                ? size.width * 0.75
+                : size.width,
+            padding: const EdgeInsets.only(top: 5, bottom: 5),
+            child: Column(
+              children: [
+                for (var i = 0; i < 5; i++)
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 5),
+                    child: ExpansionTile(
+                      initiallyExpanded: false,
+                      collapsedBackgroundColor: colorDarkMidGround,
+                      backgroundColor: colorDarkMidGround,
+                      leading: const Icon(
+                        FontAwesomeIcons.bookmark,
+                        color: colorTextPrimary,
+                        size: 30,
                       ),
+                      title: const Text(
+                        "Associate Software Engineer",
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontFamily: fontFamilySFPro,
+                          color: colorTextPrimary,
+                        ),
+                      ),
+                      subtitle: const Text(
+                        "Google In.",
+                        style: TextStyle(
+                          fontFamily: fontFamilySFPro,
+                          color: colorTextPrimary,
+                        ),
+                      ),
+                      trailing: Icon(
+                        _index == i && _isExpanded
+                            ? Icons.arrow_circle_up
+                            : Icons.arrow_circle_down,
+                        color: colorTextPrimary,
+                        size: 30,
+                      ),
+                      onExpansionChanged: (bool expanded) {
+                        setState(() {
+                          _index = i;
+                          _isExpanded = expanded;
+                        });
+                      },
+                      children: const <Widget>[
+                        BookmarkedJobBodyCard(
+                          companyLogo:
+                              "https://www.freepnglogos.com/uploads/google-logo-png/google-logo-icon-png-transparent-background-osteopathy-16.png",
+                          companyName: "Google In.",
+                          description:
+                              "A job description not only describes the position’s responsibilities, it sets the foundation for recruiting, developing and retaining talent and also sets the stage for optimum work performance by clarifying responsibilities, expected results, and evaluation of performance. It is also an important component to maintaining an equitable compensation system and ensuring legal compliance. The document should be revisited and updated in line with the annual performance evaluation cycle.",
+                          position: "Associate Software Engineer",
+                          postImage:
+                              "https://templates.mediamodifier.com/5e9709395e1d70189ea21cd1/job-posting-linkedin-post-template.jpg",
+                          qualifications: "Test",
+                          salary: "45000",
+                          type: "Full Time",
+                        ),
+                      ],
                     ),
-                ],
-              )),
+                  ),
+              ],
+            ),
+          ),
         ),
       ),
     );
